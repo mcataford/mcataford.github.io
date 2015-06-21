c1d4c88c2c436c8b5dd2a8a5e73466e3d40da8ec
Marc Cataford <c.marcandre@gmail.com>
2015-06-21 01:08:18 -0400

    Fix.

--- js/fx.js
+++ js/fx.js
@@ -1,4 +1,15 @@
+//Pageload animations.
 $(window).ready(function() {
   $("#header_title").fadeIn(800);
   $("#header_blurb").delay(300).fadeIn(800);
+  $("#skillset .section_header").delay(1100).animate({width:"show"},800);
+  $("#experience .section_header").delay(1100).animate({width:"show"},800);
+  $("#projects .section_header").delay(1100).animate({width:"show"},800);
 });
+
+//Expanding sections.
+$(".section_header h1").click(function() {
+  console.log("Yo");
+});
+
+
