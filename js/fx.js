cfbdf4d914b3eef57b27aa47c2865dd7e5e5d910
Marc Cataford <c.marcandre@gmail.com>
2015-06-22 00:42:34 -0400

    Adjusted fadeIn.

--- js/fx.js
+++ js/fx.js
@@ -6,6 +6,9 @@ $(window).ready(function() {
   $("#main_menu_skillset .menu_title").delay(1900).fadeIn(800);
   $("#main_menu_experience .menu_title").delay(2200).fadeIn(800);
   $("#main_menu_projects .menu_title").delay(2500).fadeIn(800);
+
+  $("#intro_blurb").delay(3300).fadeIn(1100);
+  $("#callmemaybe").delay(3300).fadeIn(1100);
   
 //Expanding sections.
   /*$(".section_header").click(function() {
