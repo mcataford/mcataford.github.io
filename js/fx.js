8cbeeb22a35b06a9d9a955aadef38660a6560fc5
Marc Cataford <c.marcandre@gmail.com>
2015-06-22 00:00:29 -0400

    Redesign.

--- js/fx.js
+++ js/fx.js
@@ -1,15 +1,16 @@
 //Pageload animations.
 $(window).ready(function() {
-  $("#header_title").fadeIn(800);
-  $("#header_blurb").delay(300).fadeIn(800);
-  $("#skillset .section_header").delay(1100).animate({width:"show"},800);
-  $("#experience .section_header").delay(1100).animate({width:"show"},800);
-  $("#projects .section_header").delay(1100).animate({width:"show"},800);
+  $("#main_title").fadeIn(1100);
+  $("#main_mugshot").delay(800).fadeIn(1100);
+  
+  $("#main_menu_skillset .menu_title").delay(1900).fadeIn(800);
+  $("#main_menu_experience .menu_title").delay(2200).fadeIn(800);
+  $("#main_menu_projects .menu_title").delay(2500).fadeIn(800);
   
 //Expanding sections.
-  $(".section_header").click(function() {
+  /*$(".section_header").click(function() {
     $(this).parent().find(".contents").toggle();
-  });
+  });*/
 });
 
 
