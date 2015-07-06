bdd1952ab1b73ca56c2c13e59c408af25a97c0e0
Marc Cataford <c.marcandre@gmail.com>
2015-07-05 21:09:44 -0400

    Support.

--- js/fx.js
+++ js/fx.js
@@ -1,25 +1,34 @@
-//Pageload animations.
-$(window).ready(function() {
-  $("#main_title").fadeIn(1100);
-  $("#main_mugshot").delay(800).fadeIn(1100);
-  
-  $("#main_menu_skillset .menu_title").delay(1900).fadeIn(800);
-  $("#main_menu_experience .menu_title").delay(2200).fadeIn(800);
-  $("#main_menu_projects .menu_title").delay(2500).fadeIn(800);
 
-  $("#intro_blurb").delay(3300).fadeIn(1100);
-  $("#callmemaybe").delay(3300).fadeIn(1100);
+/*
+ * On load block
+ * Setting up the initial view.
+ */
+$(window).ready(function() {
+  $('#main_title').fadeIn(1100);
+  $('#main_mugshot').delay(800).fadeIn(1100);
   
-//Expanding sections.
- /* $(".menu_title").click(function() {
-    $(this).parent().find(".menu_contents").toggle();
-  });*/
+  $('#main_menu_skillset .menu_title').delay(1900).fadeIn(800);
+  $('#main_menu_experience .menu_title').delay(2200).fadeIn(800);
+  $('#main_menu_projects .menu_title').delay(2500).fadeIn(800);
+  $('#main_menu_blog .menu_title').delay(2800).fadeIn(800);
+
+  $('#intro_blurb').delay(3300).fadeIn(1100);
+  $('#callmemaybe').delay(3300).fadeIn(1100);
+  
 });
 
-//Click event handlers.
+
+/*
+ * Click event handlers
+ */
 $(document).on('click','#main_menu_skillset', function() {
   $('#intro_blurb').toggle();
   $('#skillset').toggle();
 });
+
+$(document).on('click','#main_menu_blog', function() {
+	$('#pagewrap').fadeOut(1000);
+	$(window).load('/blogging/');
+})
 
 
