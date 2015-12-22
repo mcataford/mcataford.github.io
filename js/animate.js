1e8d4668ad3dd203601d03bb51a1b57a19e669ca
Marc Cataford <c.marcandre@gmail.com>
2015-12-22 00:59:56 -0500

    JS up

---
+++ js/animate.js
@@ -0,0 +1,19 @@
+$(window).ready(function() {
+    $('#page').fullpage({
+        anchors:['landing', 'resume', 'projects','getInTouch']
+    });
+
+    var period = "<span style='color:#000;font-weight:normal'>.</span>";
+  	var typedStrings = ["code"+period, "learn new things"+period, "tinker"+period, "communicate"+period, "teach"+period, "solve problems"+period, "get things done"+period];
+
+  	$("#typedWords").typed({
+    	strings: typedStrings,
+    	typeSpeed:0,
+    	backSpeed:0,
+    	backDelay: 1500,
+    	showCursor: true,
+    	cursorChar: "_",
+    	loop: true,
+    	contentType: 'html'
+  	});
+  });
